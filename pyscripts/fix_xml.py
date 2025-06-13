dec976ea567f35403c838174082a57ed87288ee7
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-06-13 12:19:38 +0200

    fix tif url

--- pyscripts/fix_xml.py
+++ pyscripts/fix_xml.py
@@ -3,6 +3,7 @@
 # files in data/editions. It uses the file name without extension
 import os
 from acdh_tei_pyutils.tei import TeiReader, ET
+import re
 
 BASENAME = "/"
 xml_path = "./data/editions"
@@ -12,12 +13,24 @@ xml_files = [f for f in os.listdir(xml_path) if
 
 prev_file = ""
 for current_file in xml_files:
+    print(f"Fixing {current_file}")
+    volume_name = re.sub(r"(WSTLA-OKA-B1-\d+-\d+-\d).*", r"\1", current_file)
     current_filepath = os.path.join(xml_path, current_file)
     xml_current = TeiReader(current_filepath)
     tei_ns = xml_current.ns_tei
     xml_current_root = xml_current.tree.getroot()
     xml_current_root.attrib[f'{{{xml_current.ns_xml.get("xml")}}}id'] = "".join(os.path.splitext(current_file))
     xml_current_root.attrib[f'{{{xml_current.ns_xml.get("xml")}}}base'] = BASENAME
+    for graphic in xml_current.any_xpath('//tei:graphic'):
+        url = graphic.get('url')
+        if re.match(r'^\d+\.tif$', url):
+            # Navigate to parent <surface> and extract @xml:id
+            surface = graphic.getparent()
+            xml_id = surface.get('{http://www.w3.org/XML/1998/namespace}id')
+            if xml_id:
+                id_suffix = xml_id.split('_')[1] if '_' in xml_id else xml_id
+                new_url = f"{volume_name}_{id_suffix}.tif"
+                graphic.set('url', new_url)
     if prev_file:
         xml_current_root.attrib['prev'] = prev_file
         prev_filepath = os.path.join(xml_path, prev_file)
